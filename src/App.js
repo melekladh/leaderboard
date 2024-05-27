ab36e73f38c040576982798ad04bc6f28de41f5f
melekladh <120518814+melekladh@users.noreply.github.com>
2024-05-27 19:13:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Leaderboard from './compenents/Leaderboard';
+import './compenents/style.css'
+function App() {
+  return (
+    <div className="App">
+      <Leaderboard/>
+    </div>
+  );
+}
+
+export default App;
